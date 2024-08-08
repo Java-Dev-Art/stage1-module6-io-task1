38503727887154bc1964e14719781a70e1cd386b
Artur Markouski <markouski.artur@student.ehu.lt>
2024-08-08 17:44:44 +0200

    feat: add sol2

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -6,4 +6,32 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 
 
-public class Fil
+public class FileReader {
+
+    public Profile getDataFromFile(File file) {
+        Profile profile = new Profile();
+        try( BufferedReader reader = new BufferedReader(new java.io.FileReader(file))) {
+            String line;
+            while ((line = reader.readLine()) != null){
+                String[] values = line.split(" ");
+                if (values[0].equalsIgnoreCase("Name:")){
+                    profile.setName(values[1]);
+                }
+                if (values[0].equalsIgnoreCase("Age:")){
+                    profile.setAge(Integer.parseInt(values[1]));
+                }
+                if (values[0].equalsIgnoreCase("Email:")){
+                    profile.setEmail(values[1]);
+                }
+                if (values[0].equalsIgnoreCase("Phone:")){
+                    profile.setPhone(Long.parseLong(values[1]));
+                }
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return profile;
+    }
+}
