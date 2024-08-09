84264572e0644a1f9cef516fce9a8267323ede2d
Artur Markouski <markouski.artur@student.ehu.lt>
2024-08-09 11:43:48 +0200

    feat: add sol6

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -10,25 +10,23 @@ public class FileReader {
 
     public Profile getDataFromFile(File file) {
         Profile profile = new Profile();
-        try( BufferedReader reader = new BufferedReader(new java.io.FileReader(file))) {
+        try (BufferedReader reader = new BufferedReader(new java.io.FileReader(file))) {
             String line;
-            while ((line = reader.readLine()) != null){
+            while ((line = reader.readLine()) != null) {
                 String[] values = line.split(" ");
-                if (values[0].equalsIgnoreCase("Name:")){
+                if (values[0].equalsIgnoreCase("Name:")) {
                     profile.setName(values[1]);
                 }
-                if (values[0].equalsIgnoreCase("Age:")){
+                if (values[0].equalsIgnoreCase("Age:")) {
                     profile.setAge(Integer.parseInt(values[1]));
                 }
-                if (values[0].equalsIgnoreCase("Email:")){
+                if (values[0].equalsIgnoreCase("Email:")) {
                     profile.setEmail(values[1]);
                 }
-                if (values[0].equalsIgnoreCase("Phone:")){
+                if (values[0].equalsIgnoreCase("Phone:")) {
                     profile.setPhone(Long.parseLong(values[1]));
                 }
             }
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
